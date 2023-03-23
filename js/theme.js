ee480fadabd2bc8a87d07db700e8505ffd1bb238
Muhammadhamd <muhammadhamdali572@gmail.com>
2023-03-23 23:35:50 +0500

    first commit

---
+++ js/theme.js
@@ -0,0 +1,6 @@
+const darkMood = document.getElementById("darkmode");
+
+darkMood.addEventListener("click",
+function () {
+    document.querySelector(".container").style = "background:#302f3e;";
+})
