7ec0318728ba63a74c0ac03251e8193d07f86645
Muhammadhamd <muhammadhamdali572@gmail.com>
2023-03-25 02:51:24 +0500

    Fasfa

--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,14 @@
-const darkMood = document.getElementById("darkmode");
+// const darkMood = document.getElementById("darkmode");
 
-darkMood.addEventListener("click",
-function () {
-    document.querySelector(".container").style = "background:#302f3e;";
+// darkMood.addEventListener("click",
+// function () {
+//     document.querySelector(".container").style = "background:#302f3e;";
+// })
+
+let darkmood = document.getElementById("darktheme"),
+ lightmood = document.getElementById("lighttheme");
+
+darkmood.addEventListener("click",
+function(){
+    
 })
